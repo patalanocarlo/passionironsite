e4f303cbe3971553b582c3ac4e6914d8b39aed4c
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-09-09 15:41:59 +0200

    Gestione schermi e fine parte navbar e testo in movimento

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Style/Homepage.css";
 import ImageHome from "../Images/HomepageImage.jpg";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebookF,  } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const HomePage = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="home-container">
       <nav className="navbar">
         <div className="logo">Passion Iron</div>
-        <ul className="nav-links">
+        <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
           <li><a href="#about">Prenotazioni</a></li>
           <li><a href="#vehicles">I nostri Coach</a></li>
           <li><a href="#contacts">Contatti</a></li>
         </ul>
         <div className="social-links">
-          <a href="#instagram" className="social-icon">📷</a>
-          <a href="#facebook" className="social-icon">📘</a>
-          <a href="#tiktok" className="social-icon">🎵</a>
+          <a href="#instagram" className="social-icon">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href="#facebook" className="social-icon">
+            <FontAwesomeIcon icon={faFacebookF} />
+          </a>
+          <a href="#email" className="social-icon">
+          <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+        </div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          ☰
         </div>
       </nav>
 
@@ -23,10 +40,10 @@ const HomePage = () => {
         <img src={ImageHome} alt="Car" />
         <div className="hero-overlay"></div>
         <div className="hero-text">Build Yourself!</div>
+    
       </div>
 
       <div className="content">
-        {/* Aggiungi qui il contenuto della tua pagina */}
         <section id="about">
           <h2>About</h2>
           <p>Content about Prenotazioni...</p>
